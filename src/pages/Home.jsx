5d47dda324e025d39312a8b1b2bff3b77ccc8614
AndriusFrl <andresfrl02@hotmail.com>
2025-10-04 20:46:30 -0500

    Colores y animaciones agregados

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+export const Home = () => {
+  return <div className ="min-h-screen ">
+
+{/* --- Tema --- */}
+
+{/* --- Fondo --- */}
+
+{/* --- NavBar --- */}
+
+{/* --- Main --- */}
+
+
+
+{/* --- Footer --- */}
+
+
+
+
+  </div>;
+}
+
